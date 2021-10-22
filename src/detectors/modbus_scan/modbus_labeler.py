63094c3ba39d474f49fc0684d5c14371de54c568
Ben Carlisle <benjamin.carlisle@perspectalabs.com>
2021-10-22 16:16:51 -0400

    Added modbus and gave up on keeping password secret

--- src/detectors/modbus_scan/modbus_labeler.py
+++ src/detectors/modbus_scan/modbus_labeler.py
@@ -1,13 +1,20 @@
+import uuid
+
 from labeler import Labeler
 
-from stix2 import Bundle, NetworkTraffic, IPv4Address
+from stix2 import Bundle, NetworkTraffic, IPv4Address, Tool, Relationship
+from init_labeler import ThreatActorLabeler
 
 class ModbusLabeler(Labeler):
+
+    modbus_id = "tool--%s" % uuid.uuid4()
 
     @classmethod
     def get_stix_data(cls, topic, data):
         source_ip = IPv4Address(value=data["_source"]["source"]["ip"])
         destination_ip = IPv4Address(value=data["_source"]["destination"]["ip"])
+
+        traffic_id = "network-traffic--%s" % uuid.uuid4()
 
         network_traffic = NetworkTraffic(
             src_ref=source_ip,
@@ -15,6 +22,25 @@ class ModbusLabeler(Labeler):
             src_port=data["_source"]["source"]["port"],
             dst_port=data["_source"]["destination"]["port"],
             protocols=["ipv4, tcp"],
+            id=traffic_id
         )
 
-        return Bundle(objects=[source_ip, destination_ip, network_traffic])
+        modbus_tool = Tool(
+            tool_types=["remote-access"],
+            name="Modbus",
+            id=ModbusLabeler.modbus_id
+        )
+
+        uses_network_traffic = Relationship(
+            source_ref=traffic_id,
+            target_ref=ModbusLabeler.modbus_id,
+            relationship_type="uses",
+        )
+
+        uses_modbus = Relationship(
+           source_ref=ThreatActorLabeler.actor_id,
+            target_ref=ModbusLabeler.modbus_id,
+            relationship_type="uses",
+        )
+
+        return Bundle(objects=[source_ip, destination_ip, network_traffic, modbus_tool, uses_network_traffic, uses_modbus])
