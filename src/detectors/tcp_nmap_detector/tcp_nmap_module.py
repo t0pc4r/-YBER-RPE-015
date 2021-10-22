3859df02b058b597be54f5e44bca5707d8619d9e
Ben Carlisle <benjamin.carlisle@perspectalabs.com>
2021-10-21 23:21:13 -0400

    Added tcp nmap module

--- src/detectors/tcp_nmap_detector/tcp_nmap_module.py
+++ src/detectors/tcp_nmap_detector/tcp_nmap_module.py
@@ -1,38 +1,39 @@
 from module import Module
 
-class NMapModule(Module):
+class TCPNMapModule(Module):
 
     def get_labeler_name(self):
-        return "nmap_labeler"
+        return "tcp_nmap_labeler"
 
     def get_topic(self):
-        return "nmap"
+        return "tcp_nmap"
     
     def start_getting_data(self, es):
         topic = self.get_topic()
         labeler = self.get_labeler()
 
         fields = [
-            ("source.ip", "destination.ip"),
-            ("id.orig_h", "id.resp_h"),
-            ("client_addr", "server_addr"),
+            ("source.ip", "destination.ip", "destination.port"),
+            ("id.orig_h", "id.resp_h", "id.resp_p"),
+            ("client_addr", "server_addr", "server_port"),
         ]
         for field in fields:
             query = self.get_dsts_query(*field)
             results = es.search(body=query, size=10000)
             for source_results in results["aggregations"]["sources"]["buckets"]:
                 for dates_results in source_results["dates"]["buckets"]:
-                    num_unique = dates_results["destinations"]["value"]
-                    if num_unique > 200:
-                        data = {
-                            "src_ip": source_results["key"],
-                            "ip_range": "192.168.51.0/24",
-                            "start_time": dates_results["key"],
-                            "end_Tme": dates_results["key"] + 1800000,
-                        }
-                        self.send_to_labeler(labeler, topic, data)
+                    for destinations_results in dates_results["destinations"]["buckets"]:
+                        num_unique = destinations_results["ports"]["value"]
+                        if num_unique > 512:
+                            data = {
+                                "src_ip": source_results["key"],
+                                "dst_ip": destinations_results["key"],
+                                "start_time": dates_results["key"],
+                                "end_Tme": dates_results["key"] + 300000,
+                            }
+                            self.send_to_labeler(labeler, topic, data)
 
-    def get_dsts_query(self, source_field, destination_field):
+    def get_dsts_query(self, source_field, destination_field, destination_port_field):
         return {
             "query": {
                 "bool": {
@@ -58,8 +59,15 @@ class NMapModule(Module):
                             },
                             "aggs": {
                                 "destinations": {
-                                    "cardinality": {
+                                    "terms": {
                                         "field": destination_field
+                                    },
+                                    "aggs": {
+                                        "ports": {
+                                            "cardinality": {
+                                                "field": destination_port_field
+                                            }
+                                        }
                                     }
                                 }
                             }
