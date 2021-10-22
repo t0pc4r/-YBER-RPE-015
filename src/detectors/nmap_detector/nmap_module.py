20ed02ee4d7a53d501d7d6a57fa729e35362aeb6
Ben Carlisle <benjamin.carlisle@perspectalabs.com>
2021-10-22 11:05:31 -0400

    Added protocol to nmap modules

--- src/detectors/nmap_detector/nmap_module.py
+++ src/detectors/nmap_detector/nmap_module.py
@@ -13,27 +13,30 @@ class NMapModule(Module):
         labeler = self.get_labeler()
 
         fields = [
-            ("source.ip", "destination.ip"),
-            ("id.orig_h", "id.resp_h"),
-            ("client_addr", "server_addr"),
+            ("source.ip", "destination.ip", "network.transport"),
+            ("zeek.notice.id.orig_h ", "zeek.notice.id.resp_h", "zeek.notice.id.note"), # no proto field was observed and no instances of zeek.notice have been observed anyway
+            ("netflow.source_ipv4_address", "netflow.destination_ipv4_address", "netflow.protocol_identifier"),
+            ("suricata.eve.src_ip", "suricata.eve.dst_ip", "suricata.eve.proto"),
         ]
         min_ips_polled = self.module_config["min_ips_polled"]
         for field in fields:
             query = self.get_dsts_query(*field)
             results = es.search(body=query, size=10000)
             for source_results in results["aggregations"]["sources"]["buckets"]:
-                for dates_results in source_results["dates"]["buckets"]:
-                    num_unique = dates_results["destinations"]["value"]
-                    if num_unique >= min_ips_polled:
-                        data = {
-                            "src_ip": source_results["key"],
-                            "ip_range": self.module_config["destination_block"],
-                            "start_time": dates_results["key"],
-                            "end_Tme": dates_results["key"] + self.module_config["time_window_minutes"] * 60000,
-                        }
-                        self.send_to_labeler(labeler, topic, data)
+                for protocols_results in source_results["protocols"]["buckets"]:
+                    for dates_results in protocols_results["dates"]["buckets"]:
+                        num_unique = dates_results["destinations"]["value"]
+                        if num_unique >= min_ips_polled:
+                            data = {
+                                "src_ip": source_results["key"],
+                                "ip_range": self.module_config["destination_block"],
+                                "protocol": protocols_results["key"],
+                                "start_time": dates_results["key"],
+                                "end_Tme": dates_results["key"] + self.module_config["time_window_minutes"] * 60000,
+                            }
+                            self.send_to_labeler(labeler, topic, data)
 
-    def get_dsts_query(self, source_field, destination_field):
+    def get_dsts_query(self, source_field, destination_field, protocol_field):
         return {
             "query": {
                 "bool": {
@@ -52,15 +55,22 @@ class NMapModule(Module):
                         "field": source_field
                     },
                     "aggs": {
-                        "dates": {
-                            "date_histogram": {
-                                "field": "@timestamp",
-                                "interval": "%dm" % self.module_config["time_window_minutes"]
+                        "protocols": {
+                            "terms": {
+                                "field": protocol_field
                             },
                             "aggs": {
-                                "destinations": {
-                                    "cardinality": {
-                                        "field": destination_field
+                                "dates": {
+                                    "date_histogram": {
+                                        "field": "@timestamp",
+                                        "interval": "%dm" % self.module_config["time_window_minutes"]
+                                    },
+                                    "aggs": {
+                                        "destinations": {
+                                            "cardinality": {
+                                                "field": destination_field
+                                            }
+                                        }
                                     }
                                 }
                             }
--- src/detectors/tcp_nmap_detector/tcp_nmap_module.py
+++ src/detectors/tcp_nmap_detector/tcp_nmap_module.py
@@ -1,3 +1,4 @@
+from typing import Protocol
 from module import Module
 
 class TCPNMapModule(Module):
@@ -13,9 +14,10 @@ class TCPNMapModule(Module):
         labeler = self.get_labeler()
 
         fields = [
-            ("source.ip", "destination.ip", "destination.port"),
-            ("id.orig_h", "id.resp_h", "id.resp_p"),
-            ("client_addr", "server_addr", "server_port"),
+            ("source.ip", "destination.ip", "destination.port", "network.transport"),
+            ("zeek.notice.id.orig_h ", "zeek.notice.id.resp_h", "zeek.notice.id.resp_p", "zeek.notice.id.note"), # no proto field was observed and no instances of zeek.notice have been observed anyway
+            ("netflow.source_ipv4_address", "netflow.destination_ipv4_address", "netflow.destination_transport_port", "netflow.protocol_identifier"),
+            ("suricata.eve.src_ip", "suricata.eve.dst_ip", "suricata.eve.dst_port", "suricata.eve.proto"),
         ]
         min_ports_polled = self.module_config["min_ports_polled"]
         for field in fields:
@@ -24,17 +26,19 @@ class TCPNMapModule(Module):
             for source_results in results["aggregations"]["sources"]["buckets"]:
                 for dates_results in source_results["dates"]["buckets"]:
                     for destinations_results in dates_results["destinations"]["buckets"]:
-                        num_unique = destinations_results["ports"]["value"]
-                        if num_unique > min_ports_polled:
-                            data = {
-                                "src_ip": source_results["key"],
-                                "dst_ip": destinations_results["key"],
-                                "start_time": dates_results["key"],
-                                "end_Tme": dates_results["key"] + self.module_config["time_window_minutes"] * 60000,
-                            }
-                            self.send_to_labeler(labeler, topic, data)
+                        for protocols_results in destinations_results["protocols"]["buckets"]:
+                            num_unique = protocols_results["ports"]["value"]
+                            if num_unique > min_ports_polled:
+                                data = {
+                                    "src_ip": source_results["key"],
+                                    "dst_ip": destinations_results["key"],
+                                    "protocol": protocols_results["key"],
+                                    "start_time": dates_results["key"],
+                                    "end_Tme": dates_results["key"] + self.module_config["time_window_minutes"] * 60000,
+                                }
+                                self.send_to_labeler(labeler, topic, data)
 
-    def get_dsts_query(self, source_field, destination_field, destination_port_field):
+    def get_dsts_query(self, source_field, destination_field, destination_port_field, protocol_field):
         return {
             "query": {
                 "bool": {
@@ -64,9 +68,16 @@ class TCPNMapModule(Module):
                                         "field": destination_field
                                     },
                                     "aggs": {
-                                        "ports": {
-                                            "cardinality": {
-                                                "field": destination_port_field
+                                        "protocols": {
+                                            "terms": {
+                                                "field": protocol_field
+                                            },
+                                            "aggs": {
+                                                "ports": {
+                                                    "cardinality": {
+                                                        "field": destination_port_field
+                                                    }
+                                                }
                                             }
                                         }
                                     }
