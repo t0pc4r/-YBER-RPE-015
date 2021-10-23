3ae4c15657c9366399405a261f21a3f71359c013
skeefe <skeefe@peratonlabs.com>
2021-10-22 23:51:37 -0400

    remove destination address from labeler since it isn't included in all records.

--- src/detectors/lease_detector/lease_labeler.py
+++ src/detectors/lease_detector/lease_labeler.py
@@ -12,13 +12,12 @@ class LeaseLabeler(Labeler):
     @classmethod
     def get_stix_data(cls, topic, data):
         source_ip = IPv4Address(value=data["_source"]["source"]["ip"])
-        destination_ip = IPv4Address(value=data["_source"]["destination"]["ip"])
+        
 
         traffic_id = "network-traffic--%s" % uuid.uuid4()
 
         network_traffic = NetworkTraffic(
             src_ref=source_ip,
-            dst_ref=destination_ip,
             src_port=data["_source"]["source"]["port"],
             dst_port=data["_source"]["destination"]["port"],
             protocols=["ipv4, udp, dhcp"],
@@ -43,4 +42,4 @@ class LeaseLabeler(Labeler):
             relationship_type="uses",
         )
 
-        return Bundle(objects=[source_ip, destination_ip, network_traffic, dhcp_tool, uses_network_traffic, uses_dhcp])
+        return Bundle(objects=[source_ip, network_traffic, dhcp_tool, uses_network_traffic, uses_dhcp])
